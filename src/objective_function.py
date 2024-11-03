c865c4b19b6c5aed53b6f752323da7749ea145c6
Shulha Fahmi <shulhafahmi@gmail.com>
2024-11-03 22:44:40 +0700

    feat: new objective function

--- src/objective_function.py
+++ src/objective_function.py
@@ -5,53 +5,46 @@ def calculate_line_sum_deviation(line_sum):
     return abs(line_sum - 315)
 
 def objective_function(cube):
-    """Calculate the objective function"""
-    total_deviation = 0
+    """Calculate the objective function based on mean difference and variance of line sums."""
+    alpha=0.5 
+    beta=0.5 
+    magic_number=315
+    line_sums = []
     
-    # Sum deviations for rows, columns, and pillars
     for i in range(5):
         for j in range(5):
-            # Row (fixed layer in the third dimension)
             row_sum = np.sum(cube[i, j, :])
-            total_deviation += calculate_line_sum_deviation(row_sum)
+            line_sums.append(row_sum)
             
-            # Column (fixed layer in the second dimension)
             column_sum = np.sum(cube[i, :, j])
-            total_deviation += calculate_line_sum_deviation(column_sum)
+            line_sums.append(column_sum)
             
-            # Pillar (fixed layer in the first dimension)
             pillar_sum = np.sum(cube[:, i, j])
-            total_deviation += calculate_line_sum_deviation(pillar_sum)
+            line_sums.append(pillar_sum)
     
-    # Sum deviations for face diagonals (diagonals on each face)
     for i in range(5):
-        # Layer's diagonals (layer's here is stack layer from low to top)
-        face_diag1_sum = np.sum([cube[i, j, j] for j in range(5)]) 
-        face_diag2_sum = np.sum([cube[i, j, 5 - 1 - j] for j in range(5)]) 
-        total_deviation += calculate_line_sum_deviation(face_diag1_sum)
-        total_deviation += calculate_line_sum_deviation(face_diag2_sum)
+        face_diag1_sum = np.sum([cube[i, j, j] for j in range(5)])
+        face_diag2_sum = np.sum([cube[i, j, 5 - 1 - j] for j in range(5)])
+        line_sums.extend([face_diag1_sum, face_diag2_sum])
         
-        # Layer's diagonals (layer's here is standing layer)
-        face_diag3_sum = np.sum([cube[j, i, j] for j in range(5)]) 
-        face_diag4_sum = np.sum([cube[j, i, 5 - 1 - j] for j in range(5)]) 
-        total_deviation += calculate_line_sum_deviation(face_diag3_sum)
-        total_deviation += calculate_line_sum_deviation(face_diag4_sum)
+        face_diag3_sum = np.sum([cube[j, i, j] for j in range(5)])
+        face_diag4_sum = np.sum([cube[j, i, 5 - 1 - j] for j in range(5)])
+        line_sums.extend([face_diag3_sum, face_diag4_sum])
         
-        # Layer's diagonals (layer's here is standing layer - another side view)
-        face_diag5_sum = np.sum([cube[j, j, i] for j in range(5)])  
+        face_diag5_sum = np.sum([cube[j, j, i] for j in range(5)])
         face_diag6_sum = np.sum([cube[j, 5 - 1 - j, i] for j in range(5)])
-        total_deviation += calculate_line_sum_deviation(face_diag5_sum)
-        total_deviation += calculate_line_sum_deviation(face_diag6_sum)
+        line_sums.extend([face_diag5_sum, face_diag6_sum])
 
-    # Sum deviations for space diagonals
     space_diag1_sum = np.sum([cube[i, i, i] for i in range(5)])
     space_diag2_sum = np.sum([cube[i, i, 5 - 1 - i] for i in range(5)])
     space_diag3_sum = np.sum([cube[i, 5 - 1 - i, i] for i in range(5)])
     space_diag4_sum = np.sum([cube[i, 5 - 1 - i, 5 - 1 - i] for i in range(5)])
+    line_sums.extend([space_diag1_sum, space_diag2_sum, space_diag3_sum, space_diag4_sum])
     
-    total_deviation += calculate_line_sum_deviation(space_diag1_sum)
-    total_deviation += calculate_line_sum_deviation(space_diag2_sum)
-    total_deviation += calculate_line_sum_deviation(space_diag3_sum)
-    total_deviation += calculate_line_sum_deviation(space_diag4_sum)
+    mean_difference = np.mean([calculate_line_sum_deviation(sum_line) for sum_line in line_sums])
+    avg_sum_line = np.mean(line_sums)
+    variance = np.mean([(sum_line - avg_sum_line) ** 2 for sum_line in line_sums])
     
-    return total_deviation
+    objective_value = alpha * mean_difference + beta * variance
+    
+    return objective_value
